abb889766c9c165933f2a49ac2af0f6b6a9746e1
Lili Cohen <lili.cohen.tv@gmail.com>
2021-01-12 18:35:40 -0800

    adding tables and insert into in file

---
+++ seed.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  dep_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE position (
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  position_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO department (id, dep_name)
+VALUES ();
+
+INSERT INTO position (id, title, salary, department_id)
+VALUES ();
+
+INSERT INTO employee (id, first_name, last_name, position_id, manager_id)
+VALUES ();
